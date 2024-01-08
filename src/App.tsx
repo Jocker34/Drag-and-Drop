73120379d25eb6877cfb54d178a8de585c33edb4
Jocker34 <kacper2162@wp.pl>
2024-01-08 17:50:57 +0100

    Use space instead tabs

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import './App.css';
 import { DraggableBlockList } from './components/DraggableBlockList';
 
 export const App = () => {
-	return (
-		<>
-			<h1 className='header'>Drag and drop list</h1>
-			<DraggableBlockList />
-		</>
-	);
+  return (
+    <>
+      <h1 className='header'>Drag and drop list</h1>
+      <DraggableBlockList />
+    </>
+  );
 };
--- src/constans.tsx
+++ src/constans.tsx
@@ -1,14 +1,14 @@
 export type Direction = 'up' | 'down';
 
 export interface Item {
-	id: number;
-	title: string;
+  id: number;
+  title: string;
 }
 
 export const mockItems: Item[] = [
-	{ id: 1, title: 'First element' },
-	{ id: 2, title: 'Second element' },
-	{ id: 3, title: 'Third element' },
-	{ id: 4, title: 'Fourth element' },
-	{ id: 5, title: 'Fifth element' },
+  { id: 1, title: 'First element' },
+  { id: 2, title: 'Second element' },
+  { id: 3, title: 'Third element' },
+  { id: 4, title: 'Fourth element' },
+  { id: 5, title: 'Fifth element' },
 ];
